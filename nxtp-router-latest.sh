024733895ccd85630812910e47fbd672708dc890
martynovalek <88688304+martynovalek@users.noreply.github.com>
2022-05-23 13:10:18 +0700

    Update and rename nxtp-router.sh to nxtp-router-latest.sh

--- nxtp-router-latest.sh
+++ nxtp-router-latest.sh
@@ -1,4 +1,4 @@
-#For router:0.2.0-alpha.10 Only
+#For latest version router
 #!/bin/bash
 exists()
 {
@@ -45,4 +45,39 @@ echo -e ' #########¶?/@§§§§§§§§§§§§§§§§§§§§§§§§§§§§
 echo -e ' ###########N±~=Ñ§§§§§§§§§§§§§§§§§§§§§§§§§§§Ñ=~±N########### '
 echo -e ' ##############N§»^I@§§§§§§§§§§§§§§§§§§§@I^»§N############## '
 echo -e ' ###################¶ë=í^;=&oXXXo&=;^í=ë¶################### '
+echo -e '\e[0m'
+sleep 1
+mkdir -p $HOME/connext
+if [ ! $Private_Key ]; then
+read -p "Insert your Private_Key from Metamask: " Private_Key
+echo 'export Private_Key='\"${Private_Key}\" >> $HOME/connext/your.key
+fi
 
+apt update && apt install git sudo unzip wget -y
+
+#install docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+#install docker-compose
+curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+#install nxtp-route
+cd $HOME/connext
+git clone https://github.com/connext/nxtp-router-docker-compose.git
+sleep 2 
+cd $HOME/connext/nxtp-router-docker-compose
+sleep 2 
+git checkout amarok
+sleep 2 
+docker pull ghcr.io/connext/router:latest
+
+#create env config key file
+cp .env.example .env
+sleep 2
+wget -O config.json https://raw.githubusercontent.com/NunoyHaxxana/nxtp-router-docker-config/main/config.json
+sleep 2
+wget -O key.yaml https://raw.githubusercontent.com/NunoyHaxxana/nxtp-router-docker-config/main/key.yaml
+sleep 2
